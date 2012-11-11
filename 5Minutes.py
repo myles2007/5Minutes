e30143f7d25596c7653121c4ea7fa9e10985618b
Myles Loffler <myles@britecore.com>
2012-11-10 20:12:36 -0600

    Adding a login button with basic behavior
    Doesn't yet allow login, and the JS is currently in the
    template file, so we'll want to move it later.

--- 5Minutes.py
+++ 5Minutes.py
@@ -1,12 +1,6 @@
 from flask import Flask, request, session, g, redirect, url_for,\
                   abort, render_template, flash
 
-# Configuration:
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
-
 app = Flask(__name__)
 app.config.from_object('config.Development')
 
