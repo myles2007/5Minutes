aa17e21eb356cee43510aded478aa912ac8f88a0
Ben Hayden <ben@britecore.com>
2012-12-11 14:48:05 -0600

    Added all models for 5Minutes.

--- init_models_button.py
+++ init_models_button.py
@@ -1,13 +1,15 @@
 #! /usr/bin/env python
 from fiveminutes import *
-from fiveminutes.models import User, Follower, Message, DailySong
+from fiveminutes.models import User, Announcement, WorkItem, WorkItemComment, DailySong
 
 if __name__ == '__main__':
     User.__table__.drop(db.engine, checkfirst=True)
     User.__table__.create(db.engine, checkfirst=True)
-    Follower.__table__.drop(db.engine, checkfirst=True)
-    Follower.__table__.create(db.engine, checkfirst=True)
-    Message.__table__.drop(db.engine, checkfirst=True)
-    Message.__table__.create(db.engine, checkfirst=True)
+    Announcement.__table__.drop(db.engine, checkfirst=True)
+    Announcement.__table__.create(db.engine, checkfirst=True)
+    WorkItem.__table__.drop(db.engine, checkfirst=True)
+    WorkItem.__table__.create(db.engine, checkfirst=True)
+    WorkItemComment.__table__.drop(db.engine, checkfirst=True)
+    WorkItemComment.__table__.create(db.engine, checkfirst=True)
     DailySong.__table__.drop(db.engine, checkfirst=True)
     DailySong.__table__.create(db.engine, checkfirst=True)
