5dc73f01134fc075cf4ccb5b55c5a19bafcc8be2
Ben Hayden <ben@britecore.com>
2012-12-12 16:50:56 -0600

    More work on DailySongs.

--- fiveminutes/__init__.py
+++ fiveminutes/__init__.py
@@ -10,6 +10,10 @@ if 'CONFIG' not in os.environ:
     os.environ['CONFIG'] = os.path.abspath(os.path.join(os.path.dirname(__name__), 'settings.cfg.template'))
 
 app.config.from_envvar('CONFIG')
+app.config['ROOT_PATH'] = os.path.split(os.path.abspath(__file__))[0]
+app.config['PATH_MAP'] = {'/js': os.path.join(app.config['ROOT_PATH'], 'static', 'js'),
+                          '/css': os.path.join(app.config['ROOT_PATH'], 'static', 'css'),
+                          '/img': os.path.join(app.config['ROOT_PATH'], 'static', 'img')}
 
 db = SQLAlchemy(app)
 mail = Mail(app)
